24904f30b67072d4bad6d82bc870de3b55cd77cd
RoberPF <49525174+RoberPF@users.noreply.github.com>
2019-04-25 12:09:45 +0200

    Update sumar.js
    Haciendo cambios porque no funciona

--- skills/sumar.js
+++ skills/sumar.js
@@ -7,7 +7,7 @@ module.exports = function (controller) {
     var numero2;
     var resultado;
 
-    controller.hears(['sumar'], 'direct_message,direct_mention', function (bot, message) {
+    controller.hears([/^sumar$/], 'direct_message,direct_mention', function (bot, message) {
 
         bot.startConversation(message, function (err, convo) {
             convo.say('Te voy a ayudar a sumar dos números, es algo muy difícil.');
