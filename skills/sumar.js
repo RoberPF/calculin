44107d69fdea1e9759adbb333dabf68765beb1a2
RoberPF <49525174+RoberPF@users.noreply.github.com>
2019-04-25 11:07:10 +0200

    Create sumar.js
    Este bot puede sumar números

---
+++ skills/sumar.js
@@ -0,0 +1,31 @@
+//
+// Enseñando al bot a sumar
+//
+module.exports = function (controller) {
+
+    var numero1;
+    var numero2;
+    var resultado;
+
+    controller.hears(['sumar'], 'direct_message,direct_mention', function (bot, message) {
+
+        bot.startConversation(message, function (err, convo) {
+            convo.say('Te voy a ayudar a sumar dos números, es algo muy difícil.');
+
+            convo.ask('Dime el primer número?', function (response, convo) {
+                numero1 = response.text;
+                //convo.say("Cool, I like '" + response.text + "' too!");
+                convo.next();
+            });
+            
+            convo.ask('Dime el segundo número?', function (response, convo) {
+                numero2 = response.text;
+                resultado = numero1 + numero2;
+                convo.say(numero1 + " + " + numero2 + " es " + resultado);
+                convo.next();
+            });
+            
+        });
+
+    });
+};
