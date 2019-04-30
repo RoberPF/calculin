21439f620e27ca4d378c8b6abf5da69181700a14
RoberPF <49525174+RoberPF@users.noreply.github.com>
2019-04-30 16:54:56 +0200

    Update help.js

--- skills/help.js
+++ skills/help.js
@@ -6,6 +6,7 @@ module.exports = function (controller) {
     controller.hears([/^ayuda$/], 'direct_message,direct_mention', function (bot, message) {
         var text = "Hola, esto es lo que sé hacer:";
         text += "\n- " + bot.appendMention(message, "sumar") + "; suma dos números";
+        text += "\n- " + bot.appendMention(message, "restar") + "; resta dos números";
         text += "\n- " + bot.appendMention(message, "ayuda") + ": te digo las cosas que se hacer";
         bot.reply(message, text);
     });
