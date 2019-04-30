e43e72bf01c4c54efb0aea3b901d33f90d6bfe21
RoberPF <49525174+RoberPF@users.noreply.github.com>
2019-04-30 17:16:18 +0200

    Update help.js

--- skills/help.js
+++ skills/help.js
@@ -5,11 +5,11 @@ module.exports = function (controller) {
 
     controller.hears([/^ayudame$/], 'direct_message,direct_mention', function (bot, message) {
         var text = "Hola, esto es lo que sé hacer:";
-        text += "\n- " + bot.appendMention(message, "sumar") + "; suma dos números";
-        text += "\n- " + bot.appendMention(message, "restar") + "; resta dos números";
-        text += "\n- " + bot.appendMention(message, "multiplicar") + "; multiplica dos números";
-        text += "\n- " + bot.appendMention(message, "dividir") + "; divide dos números";
-        text += "\n- " + bot.appendMention(message, "ayuda") + ": te digo las cosas que se hacer";
+        text += "\n- " + bot.appendMention(message, "suma") + "; suma dos números";
+        text += "\n- " + bot.appendMention(message, "resta") + "; resta dos números";
+        text += "\n- " + bot.appendMention(message, "multiplica") + "; multiplica dos números";
+        text += "\n- " + bot.appendMention(message, "divide") + "; divide dos números";
+        text += "\n- " + bot.appendMention(message, "ayudame") + ": te digo las cosas que se hacer";
         bot.reply(message, text);
     });
 }
