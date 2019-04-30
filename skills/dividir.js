092144011370fbb9974b29bf322a30d89f0fadd5
RoberPF <49525174+RoberPF@users.noreply.github.com>
2019-04-30 17:15:17 +0200

    Update dividir.js

--- skills/dividir.js
+++ skills/dividir.js
@@ -7,7 +7,7 @@ module.exports = function (controller) {
     var numero2 = 0;
     var resultado = 0;
 
-    controller.hears([/^dividir$/], 'direct_message,direct_mention', function (bot, message) {
+    controller.hears([/^divide$/], 'direct_message,direct_mention', function (bot, message) {
 
         bot.startConversation(message, function (err, convo) {
             convo.say('Te voy a ayudar a dividir dos números.');
