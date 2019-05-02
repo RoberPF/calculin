94b7f7a431ad704b7a73fe982f6ddeea7eb63a70
RoberPF <49525174+RoberPF@users.noreply.github.com>
2019-05-02 08:12:38 +0200

    Update z-fallback.js

--- skills/z-fallback.js
+++ skills/z-fallback.js
@@ -5,7 +5,7 @@ module.exports = function (controller) {
 
     controller.hears([".*"], 'direct_message,direct_mention', function (bot, message) {
         var mardown = "Lo siento, no entiendo lo que quieres decir.<br/>Intenta con "
-            + bot.appendMention(message, "ayuda");
+            + bot.appendMention(message, "ayudame");
             
         bot.reply(message, mardown);
     });
